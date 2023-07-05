3f333bdd352d3369c580d7c22c054a659c399cbb
MIGUEL <benedetty321@gmail.com>
2023-07-04 21:57:05 -0500

    informacion

---
+++ mensaje/info.js
@@ -0,0 +1,124 @@
+var mensajeVisible = false;
+
+function mostrarOcultarMensaje() {
+  var mensajeElement = document.getElementById("mensaje");
+  
+  if (mensajeVisible) {
+    mensajeElement.style.display = "none";
+    mensajeVisible = false;
+  } else {
+    mensajeElement.textContent = "Puede descarga windows en formato (ISO) en su pagina oficial o en mediafire";
+    mensajeElement.style.display = "block";
+    mensajeElement.style.color = "white";
+    mensajeVisible = true;
+  }
+}
+
+
+
+function informacionlinux() {
+  var mensajeElement = document.getElementById("linux");
+  
+  if (mensajeVisible) {
+    mensajeElement.style.display = "none";
+    mensajeVisible = false;
+  } else {
+    mensajeElement.textContent = "Descarga linux en formato (ISO) rapido  en su pagina oficial o en mediafire";
+    mensajeElement.style.display = "block";
+    mensajeElement.style.color = "white";
+    mensajeVisible = true;
+  }
+}
+
+
+function  informacionvisual() {
+  var mensajeElement = document.getElementById("visual");
+  
+  if (mensajeVisible) {
+    mensajeElement.style.display = "none";
+    mensajeVisible = false;
+  } else {
+    mensajeElement.textContent = "Descarga visual studio code en su pagina oficial o en mediafire y crea un mundo mejor";
+    mensajeElement.style.display = "block";
+    mensajeElement.style.color = "white";
+    mensajeVisible = true;
+  }
+}
+
+
+
+function informacionventoy() {
+  var mensajeElement = document.getElementById("ventoy");
+  
+  if (mensajeVisible) {
+    mensajeElement.style.display = "none";
+    mensajeVisible = false;
+  } else {
+    mensajeElement.textContent = "Descarga vento y mobalivecd y convierte tu usb en la mejor multisistema que hay ";
+    mensajeElement.style.display = "block";
+    mensajeElement.style.color = "white";
+    mensajeVisible = true;
+  }
+}
+
+function informacionrufus() {
+  var mensajeElement = document.getElementById("rufus");
+  
+  if (mensajeVisible) {
+    mensajeElement.style.display = "none";
+    mensajeVisible = false;
+  } else {
+    mensajeElement.textContent = "Descarga rufus al instante para bootear tus usb rapido como flash";
+    mensajeElement.style.display = "block";
+    mensajeElement.style.color = "white";
+    mensajeVisible = true;
+  }
+}
+
+function informacionoffice() {
+  var mensajeElement = document.getElementById("office");
+  
+  if (mensajeVisible) {
+    mensajeElement.style.display = "none";
+    mensajeVisible = false;
+  } else {
+    mensajeElement.textContent = "Obten el Servicio Office 365 para aumentar la eficiencia facil y rapido";
+    mensajeElement.style.display = "block";
+    mensajeElement.style.color = "white";
+    mensajeVisible = true;
+  }
+}
+
+
+function informacionwifi() {
+  var mensajeElement = document.getElementById("3");
+  
+  if (mensajeVisible) {
+    mensajeElement.style.display = "none";
+    mensajeVisible = false;
+  } else {
+    mensajeElement.textContent = "Cuando necesites los driver de internet para tu pc , no te olvides de 3DPNet ";
+    mensajeElement.style.display = "block";
+    mensajeElement.style.color = "white";
+    mensajeVisible = true;
+  }
+}
+
+function informacionactivador() {
+  var mensajeElement = document.getElementById("activador");
+  
+  if (mensajeVisible) {
+    mensajeElement.style.display = "none";
+    mensajeVisible = false;
+  } else {
+    mensajeElement.textContent = "Para cuando no tengas para licencias de windows recuendas que eres pobre ";
+    mensajeElement.style.display = "block";
+    mensajeElement.style.color = "white";
+    mensajeVisible = true;
+  }
+}
+
+
+
+
+
